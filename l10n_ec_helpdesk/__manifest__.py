36b0188262f3d84c1efcdb3629ec5bb8eecf70e6
Juan J Arosemena <40127035+jjac111@users.noreply.github.com>
2020-02-12 22:26:20 -0500

    Added helpdesk addition
    Heldesk ticket name is auto formatted.
    Purchase Orders now can include a Ticket.

---
+++ l10n_ec_helpdesk/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_ec_helpdesk",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'helpdesk',
+        'purchase',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/helpdesk_views.xml',
+    ],
+}
