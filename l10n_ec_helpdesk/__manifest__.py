56e2eb59211a8563063ef644412c3c43be91bd93
Juan J Arosemena <40127035+jjac111@users.noreply.github.com>
2020-03-16 17:53:58 -0500

    Update to work

--- l10n_ec_helpdesk/__manifest__.py
+++ l10n_ec_helpdesk/__manifest__.py
@@ -24,7 +24,6 @@
         'base',
         'helpdesk',
         'purchase',
-		'stock',
     ],
 
     # always loaded
--- l10n_ec_helpdesk/models/helpdesk.py
+++ l10n_ec_helpdesk/models/helpdesk.py
@@ -6,99 +6,99 @@ from datetime import date
 
 
 class HelpdeskTicket(models.Model):
-	_inherit = 'helpdesk.ticket'
+    _inherit = 'helpdesk.ticket'
 
-	# ---------------------
-	# DEFINICION DE CAMPOS
-	# ---------------------
-	
-	x_datetime_en_proceso = fields.datetime(
-		string='Fecha de inicio En Proceso',
-		store=True,
-	)
-	x_datetime_por_confirmar = fields.datetime(
-		string='Fecha de inicio Por Confirmar',
-		store=True,
-	)
-	x_datetime_resuelto = fields.datetime(
-		string='Fecha de inicio Resuelto',
-		store=True,
-	)
-	
-	x_sla_failed_notify = fields.boolean(
-		string='Notificado por SLA fallado',
-		store=True,
-	)
-	x_sla_over_failed_notify = fields.boolean(
-		string='Notificado por SLA sobre fallado',
-		store=True,
-	)
-	x_sla_to_fail_notify = fields.boolean(
-		string='Notificado por SLA por fallar',
-		store=True,
-	)
+    # ---------------------
+    # DEFINICION DE CAMPOS
+    # ---------------------
+    
+    x_datetime_en_proceso = fields.Datetime(
+        string='Fecha de inicio En Proceso',
+        store=True,
+    )
+    x_datetime_por_confirmar = fields.Datetime(
+        string='Fecha de inicio Por Confirmar',
+        store=True,
+    )
+    x_datetime_resuelto = fields.Datetime(
+        string='Fecha de inicio Resuelto',
+        store=True,
+    )
+    
+    x_sla_failed_notify = fields.Boolean(
+        string='Notificado por SLA fallado',
+        store=True,
+    )
+    x_sla_over_failed_notify = fields.Boolean(
+        string='Notificado por SLA sobre fallado',
+        store=True,
+    )
+    x_sla_to_fail_notify = fields.Boolean(
+        string='Notificado por SLA por fallar',
+        store=True,
+    )
 
-	# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	# –––––––––––––––––––––––––––––––––––––––––––––––––––––– @api ––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	@api.onchange('partner_id', 'ticket_type_id')
-	def _nombre_ticket(self):
-	
-		self.name = str('' if not self.partner_id.name else self.partner_id.name) + ' | ' + str('' if not self.ticket_type_id.name else self.ticket_type_id.name)
-								
-		
+    # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    # –––––––––––––––––––––––––––––––––––––––––––––––––––––– @api ––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    @api.onchange('partner_id', 'ticket_type_id')
+    def _nombre_ticket(self):
+    
+        self.name = str('' if not self.partner_id.name else self.partner_id.name) + ' | ' + str('' if not self.ticket_type_id.name else self.ticket_type_id.name)
+                                
+        
 
 
 
 
 
 class PurchaseOrder(models.Model):
-	_inherit = "purchase.order"
+    _inherit = "purchase.order"
 
-	# ---------------------
-	# DEFINICION DE CAMPOS
-	# ---------------------
-	
-	partner_ref_new = fields.Many2one(
-		'helpdesk.ticket',
-		string='Ticket',
-		help='Ticket de servicio creado en Helpdesk',
-		store=True,
-	)
-	
-	x_studio_cliente_final = fields.Many2one(
-		'res.partner',
-		string='Cliente Final',
-		help='Contacto de cliente al cual realizar entrega.',
-		store=True,
-	)
-	
-	
+    # ---------------------
+    # DEFINICION DE CAMPOS
+    # ---------------------
+    
+    partner_ref_new = fields.Many2one(
+        'helpdesk.ticket',
+        string='Ticket',
+        help='Ticket de servicio creado en Helpdesk',
+        store=True,
+    )
+    
+    x_studio_cliente_final = fields.Many2one(
+        'res.partner',
+        string='Cliente Final',
+        help='Contacto de cliente al cual realizar entrega.',
+        store=True,
+    )
+    
+    
 
-	# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	# –––––––––––––––––––––––––––––––––––––––––––––––––––––– @api ––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	@api.onchange('partner_ref_new')
-	def _vendor_ref(self):
+    # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    # –––––––––––––––––––––––––––––––––––––––––––––––––––––– @api ––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    @api.onchange('partner_ref_new')
+    def _vendor_ref(self):
 
-		self.partner_ref = str(self.partner_ref_new.name) + ' (#' + str(self.partner_ref_new.id) + ')'
-		
-		
+        self.partner_ref = str(self.partner_ref_new.name) + ' (#' + str(self.partner_ref_new.id) + ')'
+        
+        
 
 
 class StockPicking(models.Model):
-	_inherit = "stock.picking"
-	
-	# ---------------------
-	# DEFINICION DE CAMPOS
-	# ---------------------
-	x_studio_cliente_final = fields.many2one(
-		'res.partner',
-		string='Cliente Final',
-		help='Contacto de cliente al cual realizar entrega.',
-		store=True,
-	)
+    _inherit = "stock.picking"
+    
+    # ---------------------
+    # DEFINICION DE CAMPOS
+    # ---------------------
+    x_studio_cliente_final = fields.Many2one(
+        'res.partner',
+        string='Cliente Final',
+        help='Contacto de cliente al cual realizar entrega.',
+        store=True,
+    )
 
-	# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	# –––––––––––––––––––––––––––––––––––––––––––––––––––––– @api ––––––––––––––––––––––––––––––––––––––––––––––––––––––
-	# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    # –––––––––––––––––––––––––––––––––––––––––––––––––––––– @api ––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
