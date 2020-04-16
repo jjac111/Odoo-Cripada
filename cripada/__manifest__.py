809d2dc452445ce9c4655ab20436a4b404fee8b3
Juan J Arosemena <40127035+jjac111@users.noreply.github.com>
2020-04-16 17:24:41 -0500

    Changed Module Name

--- cripada/__manifest__.py
+++ cripada/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_ec_helpdesk",
+    'name': "Personalizaciones Cripada",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Juan Javier Arosemena",
+    'website': "http://www.jjac.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,11 +24,13 @@
         'base',
         'helpdesk',
         'purchase',
+		'stock',
+		'mrp'
     ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/helpdesk_views.xml',
+        'views/all_views.xml',
     ],
 }
