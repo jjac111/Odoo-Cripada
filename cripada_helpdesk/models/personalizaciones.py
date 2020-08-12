3157a5dea2f76d5f1435d04ef8581e8cfc18380d
Juan J Arosemena <jarosemena@libelulasoft.com>
2020-08-11 21:07:30 -0500

    Update personalizaciones.py

--- cripada_helpdesk/models/personalizaciones.py
+++ cripada_helpdesk/models/personalizaciones.py
@@ -119,13 +119,14 @@ class HelpdeskTeam(models.Model):
 	def url_grafana(self):
 		# thread = threading.Thread(target=extract, args=(self))
 		# thread.start()
-		return {'warning': {'title': 'Warning Title Here', 'message': 'Your warning message here'}}
-		# return {
-			# 'name'     : 'Go to website',
-            # 'type'     : 'ir.actions.act_url',
-            # 'target'   : 'new',
-            # "url": "http://138.128.244.200:3000"
-        # }
+		extract(self)
+		
+		return {
+			'name'     : 'Go to website',
+            'type'     : 'ir.actions.act_url',
+            'target'   : 'new',
+            "url": "http://138.128.244.200:3000"
+        }
 	
 
 class PurchaseOrder(models.Model):
