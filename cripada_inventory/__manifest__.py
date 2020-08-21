30a78da6ae930eaaf894f06481f28d4b22fb8fb7
Juan J Arosemena <jarosemena@libelulasoft.com>
2020-08-20 23:00:21 -0500

    Update __manifest__.py

--- cripada_inventory/__manifest__.py
+++ cripada_inventory/__manifest__.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Cripada - Inventory",
+	'name': "Cripada - Inventory",
 
-    'summary': """
-        Módulo de personalizaciones de Purchase, Sale, y Stock para Cripada S.A.""",
+	'summary': """
+		Módulo de personalizaciones de Purchase, Sale, y Stock para Cripada S.A.""",
 
-    'description': """
-        Este módulo incopora las personalizaciones requeridas por Cripada para el funcionamiento de Odoo.
-    """,
+	'description': """
+		Este módulo incopora las personalizaciones requeridas por Cripada para el funcionamiento de Odoo.
+	""",
 
-    'author': "Juan Javier Arosemena",
-    'website': "http://www.jarosemena.com",
+	'author': "Juan Javier Arosemena",
+	'website': "http://www.jarosemena.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+	# Categories can be used to filter modules in modules listing
+	# Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+	# for the full list
+	'category': 'Uncategorized',
+	'version': '0.1',
 
-    # any module necessary for this one to work correctly
-    'depends': [
-        'base',
-		'res.partner',
-        'purchase',
+	# any module necessary for this one to work correctly
+	'depends': [
+		'base',
+		'contacts',
+		'purchase',
 		'stock',
 		'sale',
-    ],
+	],
 
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/all_views.xml',
-    ],
+	# always loaded
+	'data': [
+		# 'security/ir.model.access.csv',
+		'views/all_views.xml',
+	],
 }
